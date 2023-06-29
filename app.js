faffd68150ca7abd3494998a7e6457d8e56ed748
Anna C4st <annaseniuta@gmail.com>
2023-06-29 17:18:31 +0100

    task 3

--- app.js
+++ app.js
@@ -38,28 +38,28 @@ function updateContent() {
 
   /*3*/
 
-//   document.addEventListener('DOMContentLoaded', getUserInformation);
+document.addEventListener('DOMContentLoaded', getUserInformation);
 
-//       function getUserInformation() {
-//         fetch('https://randomuser.me/api/') 
-//           .then((response) => response.json())
-//           .then((data) => {
-//             const userDetails = document.getElementById('userDetails');
-//             userDetails.innerHTML = `
-//               <img src="${data.avatar}" alt="User Avatar">
-//               <p>Name: ${data.name}</p>
-//               <p>Age: ${data.age}</p>
-//               <p>Gender: ${data.gender}</p>
-//               <p>Country: ${data.country}</p>
-//               <p>Login: ${data.login}</p>
-//               <p>Password: ${data.password}</p>
-//               <p>Email: ${data.email}</p>
-//             `;
-//           })
-//           .catch((error) => {
-//             console.error('Error:', error);
-//             const userDetails = document.getElementById('userDetails');
-//             userDetails.innerHTML = 'Error occurred while fetching user information.';
-//           });
-//       }
+    function getUserInformation() {
+        fetch('https://randomuser.me/api/') 
+          .then((response) => response.json())
+          .then((data) => {
+            const userDetails = document.getElementById('userDetails');
+            userDetails.innerHTML = `
+              <img src="${data.avatar}" alt="User Avatar">
+              <p>Name: ${data.name}</p>
+              <p>Age: ${data.age}</p>
+              <p>Gender: ${data.gender}</p>
+              <p>Country: ${data.country}</p>
+              <p>Login: ${data.login}</p>
+              <p>Password: ${data.password}</p>
+              <p>Email: ${data.email}</p>
+            `;
+          })
+          .catch((error) => {
+            console.error('Error:', error);
+            const userDetails = document.getElementById('userDetails');
+            userDetails.innerHTML = 'Error occurred while fetching user information.';
+          });
+      }
 
