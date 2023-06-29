92f88c641b06001afc9f40e7a860dba161fad5d5
Anna C4st <annaseniuta@gmail.com>
2023-06-29 17:11:30 +0100

    commit

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-// function updateContent() {
-//     const updateButton = document.getElementById('updateButton');
-//     let date = new Date();
-//     updateButton.textContent = `Your vote accepted: ${date}`;
+function updateContent() {
+    const updateButton = document.getElementById('updateButton');
+    let date = new Date();
+    updateButton.textContent = `Your vote accepted: ${date}`;
 
-//     fetch('/getData')
-//       .then((response) => response.text())
-//       .then((data) => {
-//         updateButton.textContent = `Your vote accepted: ${date}`;
-//       })
-//       .catch((error) => {
-//         console.error('Error:', error);
-//         updateButton.textContent = 'Error occurred';
-//       });
-//   }
+    fetch('/getData')
+      .then((response) => response.text())
+      .then((data) => {
+        updateButton.textContent = `Your vote accepted: ${date}`;
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+        updateButton.textContent = 'Error occurred';
+      });
+  }
 
 
   /*2*/
   function loadAuthors() {
     const authorList = document.getElementById('authorList');
-    authorList.innerHTML = ''; // Clear the previous list
+    authorList.innerHTML = ''; 
 
     fetch('/books.json')
       .then((response) => response.json())
@@ -34,3 +34,32 @@
         authorList.innerHTML = 'Error occurred';
       });
   }
+
+
+  /*3*/
+
+//   document.addEventListener('DOMContentLoaded', getUserInformation);
+
+//       function getUserInformation() {
+//         fetch('https://randomuser.me/api/') 
+//           .then((response) => response.json())
+//           .then((data) => {
+//             const userDetails = document.getElementById('userDetails');
+//             userDetails.innerHTML = `
+//               <img src="${data.avatar}" alt="User Avatar">
+//               <p>Name: ${data.name}</p>
+//               <p>Age: ${data.age}</p>
+//               <p>Gender: ${data.gender}</p>
+//               <p>Country: ${data.country}</p>
+//               <p>Login: ${data.login}</p>
+//               <p>Password: ${data.password}</p>
+//               <p>Email: ${data.email}</p>
+//             `;
+//           })
+//           .catch((error) => {
+//             console.error('Error:', error);
+//             const userDetails = document.getElementById('userDetails');
+//             userDetails.innerHTML = 'Error occurred while fetching user information.';
+//           });
+//       }
+
